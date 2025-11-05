c3ec22974bd76cf982ecfe7f5a67e7d1f8950a31
Alejandro Barrera López <93299015+vakdev@users.noreply.github.com>
2025-11-04 23:50:57 -0600

    Add static methods to VentaModel class

--- models/venta_moodel.py
+++ models/venta_moodel.py
@@ -1,7 +1,12 @@
 class VentaModel:
-    def __init__(self):
-        pass
+    @staticmethod
     def agregar_venta(cliente_id, usuario_id, productos):pass
+        
+    @staticmethod
     def obtener_ventas(fecha_inicio=None, fecha_fin=None):pass
+        
+    @staticmethod
     def obtener_detalle_venta(venta_id):pass
+        
+    @staticmethod
     def eliminar_venta(venta_id):pass
